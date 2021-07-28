1a89ebe993d3b7fb86363b029306f6df45d447be
Jordan <acey1jordan@gmail.com>
2021-07-27 19:14:26 -0600

    Buttons on same line as players

---
+++ server/players.js
@@ -0,0 +1,27 @@
+const players = [
+    {
+        "name": 'Mike Conley',
+        "position": "PG"
+    },
+    {
+        "name": "Donovon Mitchell",
+        "position": "SG"
+    },
+    {
+        "name": "Bojan Bogdonovic",
+        "position": "SF"
+    },
+    {
+        "name": "Royce O' Neal",
+        "position": "PF"
+    },
+    {
+        "name": "Rudy Gobert",
+        "position": "C"
+    }
+]
+
+module.exports = {
+    players,
+}
+// export default players;
--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import Buttons from './Components/buttons/Buttons'
 import './App.css';
 
 class App extends Component {
     constructor() {
       super();
-
+      
       this.state = {
-
+        players: [],
       }
     }
 
     componentDidMount(){
-      // axios.get('')
-      // .then((res) => console.log(res))
-      // .catch((err) => console.log(err))
+      axios.get('/api/players')
+      .then(({ data }) => this.setState({ players: data }))
+      .catch((err) => console.log(err))
     }
 
   
   render (){
   return (
-    <div className="App">
+      <div className="App">
+    <div>
       <header className="App-header">
+      {this.state.players.map((player) => {
+        return (
+          <div className="player-list">{player.name}<Buttons /> </div>
+        )
+        })}
         <Buttons />
         <Buttons />
         <Buttons />
         <Buttons />
         <Buttons />
-       
-      </header>
+        </header>
+      </div>
     </div>
-  );
-  }
+  )}
   }
 
 export default App;
